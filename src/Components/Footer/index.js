296715782643354128e785a3909c71dc652981d3
bebossi <bernardobossi77@gmail.com>
2023-02-15 10:22:27 -0300

    Adicionando footer

---
+++ src/Components/Footer/index.js
@@ -0,0 +1,29 @@
+import style from "./style.module.css";
+import { Link } from "react-router-dom";
+
+export function Footer() {
+  return (
+      <footer className={style.footer}>
+         <ul>
+        <h2>Produced by</h2>
+        <li>Bernardo Bossi</li>
+        <li>Marco Antonio</li>
+      </ul>
+      <ul>
+        <h2>Contact Us</h2>
+        <Link to="https://www.linkedin.com/in/bernardo-bossi-87433a211/" >
+          <li>Linkedin Bernardo</li>
+        </Link>
+        <Link to="https://www.linkedin.com/in/marco-antonio-oliveira-gon%C3%A7alves-314515123/" >
+          <li>Linkedin Marco</li>
+        </Link>
+      </ul>
+     
+      <ul>
+        <h2>Developed at Ironhack</h2>
+        <li>Web Dev class 94</li>
+        <li>Jan - Mar 2023</li>
+      </ul>
+    </footer>
+  );
+}
